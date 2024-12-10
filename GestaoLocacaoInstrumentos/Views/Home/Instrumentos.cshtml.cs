ac28bfcdb8b8f9a68d7ec6d1c7ebddd1675565e7
Matheus Rossetti da Silva <matheus.silva@numeria.com.br>
2024-12-10 15:32:52 -0300

    Setimo commit

---
+++ GestaoLocacaoInstrumentos/Views/Home/Instrumentos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestaoLocacaoInstrumentos.Views.Home
+{
+    public class InstrumentosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
