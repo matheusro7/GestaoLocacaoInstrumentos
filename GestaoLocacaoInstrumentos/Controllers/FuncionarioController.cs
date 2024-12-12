5dc6df0a534e3b82ab7db49706ffc0406dac8a10
Matheus Rossetti da Silva <matheus.silva@numeria.com.br>
2024-12-12 17:55:07 -0300

    Funcionario commit

--- GestaoLocacaoInstrumentos/Controllers/FuncionarioController.cs
+++ GestaoLocacaoInstrumentos/Controllers/FuncionarioController.cs
@@ -38,5 +38,41 @@ namespace GestaoLocacaoInstrumentos.Controllers
             }
             return View(funcionario);
         }
+
+        // Exibir a página de confirmação de exclusão
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var funcionario = await _context.Funcionarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (funcionario == null)
+            {
+                return NotFound();
+            }
+
+            return View(funcionario);
+        }
+
+        // Realizar a exclusão
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var funcionario = await _context.Funcionarios.FindAsync(id);
+            if (funcionario != null)
+            {
+                _context.Funcionarios.Remove(funcionario);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+
+
+
+        }
     }
 }
--- GestaoLocacaoInstrumentos/wwwroot/js/site.js
+++ GestaoLocacaoInstrumentos/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const deleteButtons = document.querySelectorAll(".btn-danger");
 
-// Write your JavaScript code.
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", function (event) {
+            if (!confirm("Tem certeza que deseja excluir este funcionário?")) {
+                event.preventDefault();
+            }
+        });
+    });
+});
